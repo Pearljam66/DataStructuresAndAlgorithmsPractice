9f25a11cef9f30cd40a99919e0bb82bf542c45f7
Sarah Clark <sarahireneclark@gmail.com>
2024-03-16 15:08:39 -0500

    time complexity function

--- DSaAPracticePlayground.playground/Pages/Complexity.xcplaygroundpage/Contents.swift
+++ DSaAPracticePlayground.playground/Pages/Complexity.xcplaygroundpage/Contents.swift
@@ -1,4 +1,18 @@
-import Foundation
+// COMPARING TIME COMPLEXITY
 
-var greeting = "Hello, playground"
+// Finds the sum of numbers from 1 to n
+// The code loops 10000 times and returns 10000.
+// It is O(n) and will take a moment to run in the playground as it counts through the loop and prints results.
+func sumFromOne (upto n: Int) -> Int {
+    var result = 0
+
+    for i in 1...n {
+        result += 1
+    }
+    print(result)
+    return result
+}
+
+sumFromOne(upto: 10000)
+
 
