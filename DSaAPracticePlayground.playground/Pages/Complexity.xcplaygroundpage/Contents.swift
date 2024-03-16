2eb72f5e735167af29bb5cbaca0edce9a612d0bf
Sarah Clark <sarahireneclark@gmail.com>
2024-03-16 15:23:40 -0500

    Fix sumFromOne function

--- DSaAPracticePlayground.playground/Pages/Complexity.xcplaygroundpage/Contents.swift
+++ DSaAPracticePlayground.playground/Pages/Complexity.xcplaygroundpage/Contents.swift
@@ -1,18 +1,17 @@
 // COMPARING TIME COMPLEXITY
 
 // Finds the sum of numbers from 1 to n
-// The code loops 10000 times and returns 10000.
+// The code loops 10000 times and returns 50005000.
 // It is O(n) and will take a moment to run in the playground as it counts through the loop and prints results.
-func sumFromOne (upto n: Int) -> Int {
+func sumFromOne(upto n: Int) -> Int {
     var result = 0
 
     for i in 1...n {
-        result += 1
+        result += i
     }
-    print(result)
+
     return result
 }
 
-sumFromOne(upto: 10000)
-
+print(sumFromOne(upto: 10000))
 
