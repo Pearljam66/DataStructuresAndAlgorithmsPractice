23e89596cb20f4b26fb8791bd77477604e94bb00
Sarah Clark <sarahireneclark@gmail.com>
2024-03-16 19:26:38 -0500

    Added some Swift Standard Library code

--- DSaAPracticePlayground.playground/Pages/SwiftStandardLibrary.xcplaygroundpage/Contents.swift
+++ DSaAPracticePlayground.playground/Pages/SwiftStandardLibrary.xcplaygroundpage/Contents.swift
@@ -1,4 +1,14 @@
-import Foundation
+// MARK: Swift Standard Library
 
-var greeting = "Hello, playground"
+// MARK: Array
 
+var people = ["Bobo", "Ricky", "Petey"]
+
+people[0]
+people[1]
+people[2]
+
+// The most efficient scenario for adding an element to an array is to append it at the end of the array.
+// This is a constant time operation or O(1), meaning the time it takes to perform this operation stays the same no matter how large the array becomes.
+people.append("Charles")
+print(people)
